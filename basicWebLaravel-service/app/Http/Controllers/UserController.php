4d0a22f71f6e01e24d72ba2082da2747b95ffcd4
Vicktor Desrony <vldcreation21@gmail.com>
2021-06-13 20:55:07 +0700

    basicWebLaravel-service/app/Http/Controllers/
    Add User Service

---
+++ basicWebLaravel-service/app/Http/Controllers/UserController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+class UserController extends Controller
+{
+    //
+    public function GetUsers(){
+        return response()->json(User::all(),200);
+    }
+
+    //just helper method
+    public function getResponse(array $arr){
+        $NTFOUND ='Not '.$arr[1].' Found';
+        $FAILED_SAVE = 'Failed to save record';
+        $FAILED_DELETE = 'Failed to delete record';
+        $SUCCESS_DELETE = 'Record was deleted';
+        $dataJson = [];
+        if($arr[0]=='NF'){
+            $dataJson = [
+                'message' => $NTFOUND
+            ];
+        }
+        else{
+            if($arr[0]=='FS')
+                $dataJson = [
+                    'message' => $FAILED_SAVE
+                ];
+            else if($arr[0]=='FD')
+                $dataJson = [
+                    'message' => $FAILED_DELETE
+                ];
+            else if($arr[0]=='SD')
+                $dataJson = [
+                    'message' => $SUCCESS_DELETE
+                ];
+        }
+        return $dataJson;
+    }
+
+    public function getUsersDetail($id){
+        $one = User::find($id);
+        if($one){
+            return response()->json($one,200);
+        }
+        $arr = array('NF','User');
+        return $this->getResponse($arr);
+    }
+
+    public function getUsersByEmail($email){
+        $one = User::where('email',$email)->first();
+        if($one){
+            return response()->json($one,200);
+        }else{
+            return response()->json([
+                'message' => 'Not user found!'
+            ],404);
+        }
+    }
+
+    public function addUser(Request $request){
+        $one = User::create(array_merge($request->all(),['password' => \Hash::make(strval(123456))]));
+        if($one){
+            return response()->json($one,201);
+        }
+        return response()->json([
+            'message'=>'Failed to save record'
+        ],404);
+    }
+
+    public function updateUser(Request $request,$id){
+        $one = User::find($id);
+        if($one){
+            $one->update($request->all());
+            return response()->json($one,201);
+        }
+        $arr = array('NF','User');
+        return $this->getResponse($arr);
+    }
+
+    public function deleteUser($id){
+        $one = User::find($id);
+        if($one){
+            $wasDelete = $one->delete();
+            if($wasDelete){
+                return response()->json([
+                    'message' => 'Record was deleted'
+                ],201);
+            }
+            $arr = array('FD');
+            return $this->getResponse($arr);
+        }
+        $arr = array('NF','User');
+        return $this->getResponse($arr);
+
+    }
+}
---
+++ basicWebLaravel-service/routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//get API
+Route::get('user','UserController@getUsers');
+Route::get('user/{id}','UserController@getUsersDetail');
+Route::get('user/byEmail/{email}','UserController@getUsersByEmail');
+
+//POST API
+Route::post('addUser','UserController@addUser');
+
+//Put API
+Route::put('updateUser/{id}','UserController@updateUser');
+
+//Delete API
+Route::delete('deleteUser/{id}','UserController@deleteUser');
